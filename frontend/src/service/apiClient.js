9fe10d34aac44fd3c2d1f1df911c705f47fc2c09
ninarada <nradja00@fesb.hr>
2025-01-09 11:24:18 +0100

    .

--- frontend/src/service/apiClient.js
+++ frontend/src/service/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:5001",
+    baseURL: "https://recipe-app-tll1.onrender.com/",
 });
 
 export default apiClient;
